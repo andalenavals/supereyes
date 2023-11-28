a600ae68a38475146688b11585d562150f419fe6
Navarro <navarro@Navarros-MacBook-Pro.local>
2023-11-28 15:18:47 +0100

    voice to text v2

--- voicetotext.py
+++ voicetotext.py
@@ -1,13 +1,21 @@
 import whisper
 import os
 
-model = whisper.load_model("base")
-audiofile=os.path.join("assets", "audio.mp3")
-result = model.transcribe(audiofile)
-print(result["text"])
 
-audio = whisper.load_audio(audiofile)
-audio = whisper.pad_or_trim(audio)
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+def get_audio_features(audiofile):
+    
+    model = whisper.load_model("base")
+    result = model.transcribe(audiofile)
+    #print(result["text"])
+
+    '''
+    audio = whisper.load_audio(audiofile)
+    audio = whisper.pad_or_trim(audio)
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    _, probs = model.detect_language(mel)
+    #print(f"Detected language: {max(probs, key=probs.get)}")
+    '''
+    return [result['text'], result['language'] ]
+    
+audiofile=os.path.join("assets", "audio.mp3")
+print(get_audio_features(audiofile))
